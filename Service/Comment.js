daf687351990347b738026d62533b9f004b0ac71
Jean-Maxime <jean-max@wanadoo.fr>
2016-10-02 16:12:18 +0200

    Add test and method Comment

--- Service/Comment.js
+++ Service/Comment.js
@@ -5,9 +5,57 @@ const Comment = require('../Database/Models/Comment');
 module.exports.findAll = () => {
   return Comment.fetchAll()
 };
+
 module.exports.findById = (idComment) => {
   return Comment.where('id', idComment).fetch()
 };
+
 module.exports.deleteById = (idComment) => {
-  return Comment.where('id', idComment).destroy();
+	return Comment.where('id', idComment).destroy()
+		.then(() => {
+			return {
+				response: true,
+				message: 'Done without error'
+			}
+		})
+		.catch((err) => {
+			return {
+				response: false,
+				message: err
+			}
+		})
+};
+
+module.exports.insert = (data) => {
+	return new Comment(data).save()
+		.then((model) => {
+			return {
+				id: model.id,
+				response: true,
+				message: 'Done without any error'
+			};
+		})
+		.catch((err) => {
+			return {
+				id: null,
+				response: false,
+				message: err
+			};
+		});
+};
+
+module.exports.update = (data) => {
+	return new Comment(data).save()
+		.then(() => {
+			return {
+				response: true,
+				message: 'Done without any error'
+			}
+		})
+		.catch((err) => {
+			return {
+				response: false,
+				message: err
+			}
+		})
 };
---
+++ test/Service/Comment.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const chai     = require('chai');
+const expect  = chai.expect;
+const commentService = require('../../Service/Comment');
+
+// data to insert in database
+const toInsert = {
+	id_article: 1,
+	content: 'contentTest',
+	visible: 1,
+	author_name: 'nameTest',
+	mail: 'mailTest'
+};
+
+// return id from insert
+let idInsert;
+
+describe('Test Service Comment', () => {
+
+	describe('Function findAll', () => {
+		it('Should return 1 comment', (done) => {
+			commentService.findAll()
+				.then((res) => {
+					const comments = res.toJSON();
+
+					expect(comments).to.be.an('array');
+					expect(comments).to.have.lengthOf(1);
+					expect(comments[0]).to.have.property('author_name', 'nameTest');
+					expect(comments[0]).to.have.property('content', 'ContentTest');
+					expect(comments[0]).to.have.property('mail', 'mailTest');
+					expect(comments[0]).to.contain.all.keys('visible', 'content', 'mail', 'author_name');
+
+					done();
+				});
+		});
+	});
+
+	describe('Function findById', () => {
+		it('Should return 1 comment', (done) => {
+			commentService.findById(1)
+				.then((res) => {
+					const comment = res.toJSON();
+
+					expect(comment).to.be.an('object');
+					expect(comment).to.have.property('author_name', 'nameTest');
+					expect(comment).to.have.property('content', 'ContentTest');
+					expect(comment).to.have.property('mail', 'mailTest');
+					expect(comment).to.contain.all.keys('visible', 'content', 'mail', 'author_name');
+
+					done();
+				});
+		});
+	});
+
+	describe('Function insert', () => {
+		it('Should return ID if success', (done) => {
+			commentService.insert(toInsert)
+				.then((response) => {
+					idInsert = response.id;
+
+					expect(response).to.be.an('object');
+					expect(response.response).to.be.a('boolean');
+					expect(response.response).to.be.true;
+					expect(response.id).to.be.a('number');
+					expect(response.message).to.be.a('string');
+
+					done();
+				});
+		});
+	});
+
+	describe('Function update', () => {
+		it('Should return message and boolean', (done) => {
+			let dataToUpdate = {
+				id: idInsert,
+				content: 'contentXXX',
+			};
+			commentService.update(dataToUpdate)
+				.then((response) => {
+					expect(response).to.be.an('object');
+					expect(response.response).to.be.a('boolean');
+					expect(response.response).to.be.true;
+					expect(response.message).to.be.a('string');
+
+					done();
+				});
+		})
+	});
+
+	describe('Function delete', () => {
+		it('Should return message and boolean', (done) => {
+			commentService.deleteById(idInsert)
+				.then((response) => {
+					expect(response).to.be.an('object');
+					expect(response.response).to.be.a('boolean');
+					expect(response.response).to.be.true;
+					expect(response.message).to.be.a('string');
+
+					done();
+				})
+		});
+	});
+
+});
